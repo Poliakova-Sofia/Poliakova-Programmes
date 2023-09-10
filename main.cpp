8179239f7b96094082d58d8185aaabbf3692a30d
Poliakova-Sofia <144430007+Poliakova-Sofia@users.noreply.github.com>
2023-09-10 17:15:45 +0300

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <iostream> 
+
+int main () // writing a phrase
+
+{ // begin
+
+    std :: cout << "Hello world"; // phrase output
+    system ("pause");
+    return 0;
+    
+} // end
+
+
+
+
+#include <iostream>
+using namespace std;
+
+int main () // calculation with one unknown term
+{ //begin
+
+    float a;  // declaration
+    
+    //input
+    cout << " a : "; 
+    cin >> a;
+    
+    //calculation
+    float res = a - 5;
+    
+    //output
+    cout << "a-5 = " << res << endl;
+    
+    system ("pause");
+    return 0;
+} //end
